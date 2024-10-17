bd5cce3260d639191b380eaa387f26188a124b64
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2024-10-17 19:06:18 +0530

    Create 14.cpp

---
+++ 14.cpp
@@ -0,0 +1,12 @@
+// C++ program to illustrate
+// use of single-line comment
+#include <iostream>
+int main()
+{
+
+    // Single line Welcome user comment
+    std::cout << "Welcome to GeeksforGeeks";
+    return 0;
+}
+
+// This code is contributed by sarajadhav12052009
