93a2ced5a887b5c81201b86b6738688ffba31826
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2024-10-17 19:08:19 +0530

    Create 17.c

---
+++ 17.c
@@ -0,0 +1,13 @@
+/* C program to illustrate 
+use of 
+multi-line comment */
+#include <stdio.h>
+int main(void)
+{
+
+    /* Multi-line Welcome user comment 
+    written to demonstrate comments
+    in C/C++ */
+    printf("Welcome to GeeksforGeeks");
+    return 0;
+}
