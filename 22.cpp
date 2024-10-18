edc01daa4d29c83f1cf747ae0242708b347d54e8
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2024-10-18 11:45:59 +0530

    Create 22.cpp

---
+++ 22.cpp
@@ -0,0 +1,15 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+// global variable
+int  global = 5;
+
+int main() {
+    // local variable with same
+    // name as that of global variable
+    int global = 2 ;
+    cout << global << endl;
+
+    return 0;
+}
