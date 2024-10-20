5d066e4ea51831f487026ac6eb59ac4658f4f15a
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2024-10-20 19:31:51 +0530

    Create 72.cpp

---
+++ 72.cpp
@@ -0,0 +1,26 @@
+// C++ program  to demonstrate syntax of switch
+#include <iostream>
+using namespace std;
+
+// Driver Code
+int main()
+{
+    char x = 'A';
+    switch (x) {
+    case 'A':
+        cout << "Choice is A";
+        break;
+    case 'B':
+        cout << "Choice is B";
+        break;
+    case 'C':
+        cout << "Choice is C";
+        break;
+    default:
+        cout << "Choice other than A, B and C";
+        break;
+    }
+    return 0;
+}
+
+//Code submitted by Susobhan AKhuli
