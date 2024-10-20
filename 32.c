a3e8789678868a1db1ac17752c9fd100ad8bd8ac
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2024-10-20 13:03:23 +0530

    Create 32.c

---
+++ 32.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    // code
+      //We cann't use endl in C
+      printf("GFG!",endl);  //So it throws an error
+      printf("GFG!");
+    return 0;
+}
+
+//Code submitted by Susobhan AKhuli
