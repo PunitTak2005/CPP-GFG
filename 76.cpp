d2c16cfd9fe116098b1728822b8cdd714454ee79
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2024-10-20 19:40:18 +0530

    Create 76.cpp

---
+++ 76.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a = 12, b = 6;
+    
+    a += b;
+    cout << "Add & Assign: "<< a <<"\n";
+    
+    a -= b;
+    cout << "Subtract & Assign: " << a << "\n";
+    
+    a *= b;
+    cout << "Multiply & Assign: " << a << "\n";
+    
+    a /= b;
+    cout << "Divide & Assign: " << a << "\n"; 
+    
+    return 0;
+}
