b5ff48db2bbc6d2b0bb8bfc453711de4ee8e0f94
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2024-10-20 13:04:13 +0530

    Create 33.cpp

---
+++ 33.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "GFG!"<<endl;  //We can use endl in C++ and it doesn't occupy any memory
+      cout << "GFG!";
+    return 0;
+}
