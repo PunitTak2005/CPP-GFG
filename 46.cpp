b7ef930d3f37008e838866b721f3ed2cbda24303
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2024-10-20 13:33:57 +0530

    Create 46.cpp

---
+++ 46.cpp
@@ -0,0 +1,19 @@
+// C++ program to demonstrate working of ceil()
+// in C/C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// Driver Code
+int main()
+{
+    double x = 1.411, y = 1.500, z = 1.611;
+    cout << ceil(x) << endl;
+    cout << ceil(y) << endl;
+    cout << ceil(z) << endl;
+
+    double a = -1.411, b = -1.500, c = -1.611;
+    cout << ceil(a) << endl;
+    cout << ceil(b) << endl;
+    cout << ceil(c) << endl;
+    return 0;
+}
