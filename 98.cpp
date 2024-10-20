12977c6180ac604e549fdb090d7e5e671317b62c
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2024-10-20 23:33:56 +0530

    Create 98.cpp

---
+++ 98.cpp
@@ -0,0 +1,12 @@
+//c++ program to sizes of data types
+#include <iostream>
+#include <limits.h>
+using namespace std;
+int main()
+
+{cout<<"Size of char:"<<sizeof(char)<<"byte"<<endl;
+cout<<"char minimum value:"<<CHAR_MIN<<endl;
+cout<<"char maximum value"<<CHAR_MAX<<endl;
+cout<<"Size of int:"<<sizeof(int)<<endl;
+cout<<"Size of short  int:"<<sizeof(short int)<<endl;
+cout<<"Size of long int:"<<sizeof(long int)<<endl;}
