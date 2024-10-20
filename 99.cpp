1a1476efc75c3abf4a22e71fa7b3939005ff965f
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2024-10-20 23:39:08 +0530

    Create 99.cpp

---
+++ 99.cpp
@@ -0,0 +1,14 @@
+//c++ program to sizes of data types
+#include <iostream>
+#include <limits.h>
+using namespace std;
+int main()
+
+{cout<<"Size of signed long int:"<<sizeof(signed long int)<<"byte"<<endl;
+cout<<"size of unsigned long int"<<sizeof(unsigned long int)<<"bytes"<<endl;
+cout<<"size of float:"<<sizeof(float)<<"bytes"<<endl;
+cout<<"Size of double:"<<sizeof(double)<<"bytes"<<endl;
+cout<<"Size of wchar_t:"<<sizeof(wchar_t)<<"bytes"<<endl;
+
+    return 0;
+}
