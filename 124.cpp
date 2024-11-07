aa2a0d74dd8e21855699fe1f89d21f7efa136d38
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2024-11-07 23:44:48 +0530

    Create 124.cpp

---
+++ 124.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{//loop from 1 to 10
+  for (int i=1;i<=10;i++){
+    //if i is equal to 6,
+    //continue to next iteration
+    //without printing
+    if(i==6)
+      continue;
+    else;
+      //otherwise print the value of i
+      cout << i<< " ";
+return 0; }}
