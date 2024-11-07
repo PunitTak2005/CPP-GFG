403a862644696f46b2c8df7a28bd1b8a5fc6e5e6
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2024-11-07 18:55:59 +0530

    Create 113.cpp

---
+++ 113.cpp
@@ -0,0 +1,12 @@
+/* C++ program to illustrate
+use of
+multi-line comment*/
+##include<iostream>
+intmain()
+{
+  /*Multi-line Welecome user comment
+  written to demonstrate comments
+  in c/c++ */
+std ::cout<<"Welcome to GeeksforGeeks";
+return 0;
+}
