44a8dc5ce6f2252f7995372c2a3d7374a724c7b7
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2024-11-07 22:38:12 +0530

    Create 117.cpp

---
+++ 117.cpp
@@ -0,0 +1,13 @@
+// Cpp program to illustrate
+// Pre-increment (or pre-decrement)
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int a = 10;
+++a = 20;
+cout <<"a=" <<a<<"\n';
+--a=10;
+cout<<'a=" << a << "\n";
+return 0;
+}
