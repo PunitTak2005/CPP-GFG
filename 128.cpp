64ecb76f760996d87bc032557ce276f06f6a593d
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2024-11-20 21:31:44 +0530

    Create 128.cpp

---
+++ 128.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // initialization expression
+    int i = 10;
+    // test expression
+    while (i < 15)  {
+        cout << "Hello world\n";
+        // update expression
+        i++;
+    }
+    return 0;
+}
