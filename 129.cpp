15a2b51908b597e68f6f06b94eef2b44dcbc7347
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2024-11-20 22:22:41 +0530

    Create 129.cpp

---
+++ 129.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i=2;// initialization expression
+    do
+    
+     {//loop body
+        cout << "Hello world\n";
+        // update expression
+        i++;
+    }while(i<1);    // test expression
+    return 0;
+}
