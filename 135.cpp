0f63448a0690a481fda8f8082343853f426618c6
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2024-11-23 17:35:38 +0530

    Create 135.cpp

---
+++ 135.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+
+void func(){
+    int age = 18;
+    cout << age;}
+
+int main() {
+    
+
+    
+
+        cout << "age is " ;
+            func();
+            
+
+return 0;
+
+
+}
