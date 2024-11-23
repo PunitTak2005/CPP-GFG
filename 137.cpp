10b2ce1ab1da620698c01db3a011f451967cf205
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2024-11-23 18:04:50 +0530

    Create 137.cpp

---
+++ 137.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+
+int main() {
+    
+    int num1 = 5 - (17*6);
+
+    int num2= 5 - 17 * 6;
+
+    int num3 = (5 -17 ) *6;
+
+    cout << "num1: " << num1 << endl ;
+    cout << "num2: " << num2 << endl ;
+    cout << "num3: " << num3 << endl ; 
+  
+    return 0;
+}
