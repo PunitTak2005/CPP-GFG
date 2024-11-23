8033664d951ed9ca3dbdf314568c3d896af5aa31
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2024-11-23 17:29:44 +0530

    Create 133.cpp

---
+++ 133.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    double x = 18;
+  
+int c =static_cast<int>(x);
+
+cout << "The integer equivalent of " << x << " is: " << c << endl;
+
+return 0;
+
+
+}
