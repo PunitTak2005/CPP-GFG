7cf29cf5983ba6bfd3c10366e9dbaec8bef9ae7c
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2024-12-30 20:48:44 +0530

    Create 172.cpp

---
+++ 172.cpp
@@ -0,0 +1,49 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+  public:
+    int findUnion(vector<int>& a, vector<int>& b) {
+        unordered_set<int> s(a.begin(), a.end()); 
+        s.insert(b.begin(), b.end());            
+        return s.size();    
+    }
+};
+
+
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // Ignore the newline character after reading t
+
+    while (t--) {
+        vector<int> a;
+        vector<int> b;
+
+        string input;
+        // For a
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            a.push_back(number);
+        }
+
+        // For b
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss2(input);
+        while (ss2 >> number) {
+            b.push_back(number);
+        }
+
+        Solution ob;
+        cout << ob.findUnion(a, b) << endl;
+        cout << '~' << endl;
+    }
+
+    return 0;
+}
