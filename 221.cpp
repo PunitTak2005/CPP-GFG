c698c5135959a880e4564ceb7f6c6cad5df5b484
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2025-02-18 00:14:49 +0530

    Create 221.cpp

---
+++ 221.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<pair<int, vector<int>>, vector<pair<int, vector<int>>>, greater<pair<int, vector<int>>>> pq;
+        int n = points.size();
+        for (int i=0; i<n; i++) {
+            int d = points[i][0]*points[i][0] + points[i][1]*points[i][1];
+            pq.push({d, points[i]});
+        }
+        
+        vector<vector<int>> res;
+        for (int i=0; i<k; i++) {
+            vector<int> point = pq.top().second;
+            pq.pop();
+            res.push_back(point);
+        }
+        
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int k;
+        cin >> k;
+        int n;
+        cin >> n;
+        vector<vector<int>> points(n, vector<int>(2));
+        for (int i = 0; i < n; i++) {
+            cin >> points[i][0] >> points[i][1];
+        }
+        Solution ob;
+        vector<vector<int>> ans = ob.kClosest(points, k);
+        sort(ans.begin(), ans.end());
+        for (const vector<int>& point : ans) {
+            cout << point[0] << " " << point[1] << endl;
+        }
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
