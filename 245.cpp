e6bf7b43895dba7af269a8a3d0316e02f6b839e8
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2025-03-11 11:57:33 +0530

    Create 245.cpp

---
+++ 245.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+
+class Solution {
+  public:
+    int countWays(int n) {
+        // your code here
+        if(n <= 2) return n;
+        int a = 1, b = 2, c;
+        for(int i = 3; i <= n; i++){
+            c = a+b;
+            a = b;
+            b = c;
+        }return b;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main() {
+    // taking total testcases
+    int t;
+    cin >> t;
+    while (t--) {
+        // taking stair count
+        int m;
+        cin >> m;
+        Solution ob;
+        cout << ob.countWays(m) << endl; // Print the output from our pre computed array
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
