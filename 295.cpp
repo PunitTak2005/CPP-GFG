a14834d66a190645b21552c262947fcd4db82b36
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2025-04-21 00:07:41 +0530

    Create 295.cpp

---
+++ 295.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    int missingNum(vector<int>& arr) {
+      
+          int sum = 0;
+        int tsum = 0;
+        for(int i = 0 ; i<arr.size() ; i++)
+        {
+            sum += arr[i];
+        }
+        for(int i = 1 ; i<=(arr.size()+1) ; i++)
+        {
+            tsum += i;
+        }
+        return (tsum-sum);
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // to ignore the newline after the integer input
+    while (t--) {
+        int n;
+        vector<int> a;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num);
+
+        Solution obj;
+        cout << obj.missingNum(a) << endl;
+        cout << "~\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
