a9e1951c74b8147d5dae313d475f73f20533ee6b
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2025-04-29 00:07:28 +0530

    Create 304.cpp

---
+++ 304.cpp
@@ -0,0 +1,132 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Link list Node */
+class Node {
+  public:
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};
+
+void printList(Node* node) {
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << "\n";
+}
+
+void freeList(struct Node* head) {
+    struct Node* temp;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        delete temp;
+    }
+}
+
+
+// } Driver Code Ends
+
+/*
+
+/*
+ 
+  Node is defined as
+  struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+class Solution
+{
+    public:
+    //Function to sort a linked list of 0s, 1s and 2s.
+    Node* segregate(Node *head) {
+        // Add code here
+        unordered_map<int, int> freq;
+        Node *curr = head;
+        int count = 0;
+        while(curr != NULL) {
+            freq[curr->data]++;
+            curr = curr->next;
+            count++;
+        }
+        
+        Node *temp = head;
+        for(int i=0; i<count; i++) {
+            if(freq[0] != 0) {
+                temp->data = 0;
+                temp = temp->next;
+                freq[0]--;
+            }
+            else if(freq[1] != 0){
+                temp->data = 1;
+                temp = temp->next;
+                freq[1]--;
+            }
+            else{
+                temp->data = 2;
+                temp = temp->next;
+                freq[2]--;
+            }
+        }
+        return head;
+    }
+};
+
+
+
+
+
+//{ Driver Code Starts.
+
+// Driver program to test above functions
+int main() {
+    int t, k;
+    cin >> t;
+    cin.ignore(); // Ignore the newline character after t
+
+    while (t--) {
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+
+        stringstream ss(input);
+        Node *head = nullptr, *tail = nullptr;
+        int x;
+
+        if (ss >> x) {
+            head = new Node(x);
+            tail = head;
+
+            while (ss >> x) {
+                tail->next = new Node(x);
+                tail = tail->next;
+            }
+
+            // Link the last node to the head if k is 1 to make it circular
+            if (k == 1) {
+                tail->next = head;
+            }
+        }
+
+        Solution ob;
+        Node* newHead = ob.segregate(head);
+        printList(newHead);
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
