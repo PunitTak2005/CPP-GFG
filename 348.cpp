7c1f42ebe04c8a70aa9644ac5278f87bb61ab8b0
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2025-06-11 01:02:04 +0530

    Create 348.cpp

---
+++ 348.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    // Function to find the length of the stack after performing operations.
+    int findLength(vector<int> color, vector<int> radius) {
+        int n = color.size();
+
+        stack<int> st;
+
+        for (int i = 0; i < n; i++) {
+
+            // If the top of the stack matches
+            // current, pop it (remove both)
+            if (!st.empty() && color[i] == color[st.top()] &&
+                radius[i] == radius[st.top()]) {
+                st.pop();
+            }
+
+            else {
+                st.push(i);
+            }
+        }
+
+        // Return the number of elements
+        // left in the stack
+        return st.size();
+    }
+};
