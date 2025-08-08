ccd23fa3190e79c899871add34c959439ce5c5b7
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2025-08-08 07:27:19 +0530

    Create 409.cpp

---
+++ 409.cpp
@@ -0,0 +1,33 @@
+class Solution {
+  public:
+    int getLPSLength(string &s) {
+        int n = s.size();
+
+        // initialize LPS array with 0s
+        vector<int> lps(n, 0);
+
+        // length of the previous longest
+        // prefix-suffix
+        int len = 0;
+        int i = 1;
+
+        while (i < n) {
+            if (s[i] == s[len]) {
+                lps[i] = ++len;
+                i++;
+            } else {
+                if (len != 0) {
+                    // Fall back in the LPS array
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+
+        // lps[n - 1] holds the result for
+        // the entire string
+        return lps[n - 1];
+    }
+};
