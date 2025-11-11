26c9710df2987f1b59b67bb6ea429c3d74475c55
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2025-11-11 15:16:23 +0530

    Create 491.cpp

---
+++ 491.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    int minSuperSeq(char *s1, char *s2) {
+    int m = strlen(s1), n = strlen(s2);
+    int dp[m + 1][n + 1];
+
+    for (int i = 0; i <= m; i++) {
+        for (int j = 0; j <= n; j++) {
+            if (i == 0)
+                dp[i][j] = j;
+            else if (j == 0)
+                dp[i][j] = i;
+            else if (s1[i - 1] == s2[j - 1])
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            else
+                dp[i][j] =
+                    1 + (dp[i - 1][j] < dp[i][j - 1] ? dp[i - 1][j] : dp[i][j - 1]);
+        }
+    }
+    return dp[m][n];
+}
+};
