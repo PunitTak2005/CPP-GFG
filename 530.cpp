c086a63ea23fd0a023017563971a5fb18fd6e07f
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2025-12-24 08:41:17 +0530

    Implement minimumBoxes function in Solution class

---
+++ 530.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minimumBoxes(vector<int>& apple, vector<int>& capacity) {
+        int totalApples = 0;
+        for (int a : apple) totalApples += a;
+        sort(capacity.begin(), capacity.end(), greater<int>());
+        int count = 0;
+        for (int c : capacity) {
+            totalApples -= c;
+            count++;
+            if (totalApples <= 0) break;
+        }
+        return count;
+    }
+};
