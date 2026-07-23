5b28c827500430b6ea8c2c14e7cb9892d32ed664
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2026-07-23 07:27:25 +0530

    Create 705.cpp

---
+++ 705.cpp
@@ -0,0 +1,33 @@
+class Solution {
+  public:
+    bool canRepresentBST(vector<int> &arr) {
+        // Create an empty stack
+        stack<int> s;
+
+        // Initialize current root as minimum possible
+        // value
+        int root = INT_MIN;
+
+        // Traverse given array
+        for (int i = 0; i < arr.size(); i++) {
+            // If we find a node who is on right side
+            // and smaller than root, return false
+            if (arr[i] < root)
+                return false;
+
+            // If pre[i] is in right subtree of stack top,
+            // Keep removing items smaller than pre[i]
+            // and make the last removed item as new
+            // root.
+            while (!s.empty() && s.top() < arr[i]) {
+                root = s.top();
+                s.pop();
+            }
+
+            // At this point either stack is empty or
+            // pre[i] is smaller than root, push pre[i]
+            s.push(arr[i]);
+        }
+        return true;
+    }
+};
